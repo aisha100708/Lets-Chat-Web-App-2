24ed734c714725c8469c3cf63995c63859d93375
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-04-14 15:11:19 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,68 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBEFBRr9LoIKa0n9_WjMZe4BcWc7WbpiJM",
+    authDomain: "let-s-chat-web-app-3ee81.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-3ee81-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-3ee81",
+    storageBucket: "let-s-chat-web-app-3ee81.appspot.com",
+    messagingSenderId: "211492660817",
+    appId: "1:211492660817:web:9029121cc89e7994017a70",
+    measurementId: "G-000TDPR43G"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  room_name = localStorage.getItem("roomname");
+  user_name = localStorage.getItem("username");
+
+  function send_msg() {
+      message = document.getElementById("send_message").value;
+      firebase.database().ref(room_name).push({
+          msg: message,
+          name: user_name,
+          like: 0
+      });
+      document.getElementById("send_message").value = "";
+  }
+
+  function logOut() {
+    window.location = "kwitter.html";
+    localStorage.removeItem("username");
+    localStorage.removeItem("roomname");
+}
+
+function getData() {
+    firebase.database().ref("/"+room_name).on('value', function(snapshot) {
+        document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function(childSnapshot) {
+            childKey = childSnapshot.key;
+            childData = childSnapshot.val();
+            if(childKey != "purpose") {
+                firebase_message_id = childKey;
+                message_data = childData;
+                //Start code
+                console.log(firebase_message_id);
+                console.log(message_data);
+                sender_name = message_data['name'];
+                sender_message = message_data['msg'];
+                sender_like = message_data['like'];
+                name_tag = "<h3>" + sender_name + "<img src='tick.png' class='user_tick' </h3> <hr>";
+                msg_tag = "<h4>" + sender_message + "</h4> <hr>";
+                like_btn = "<button id='"+firebase_message_id+"' class='btn btn-primary' onclick='update_like()' value='"+sender_like+"'> Like: '"+sender_like+"' <hr>";
+                span_likeTag = "<span class='gluphicon glyphicon-thumbs-up'> </span> </button>"
+                row = name_tag + msg_tag + like_btn;
+                document.getElementById("output").innerHTML += row;
+                //End code
+            }
+        });
+    });
+}
+getData();
+
+function update_like(message_id) {
+    console.log(message_id);
+    new_like = document.getElementById(message_id).value;
+    no_of_likes = Number(new_like) + 1;
+    firebase.database().ref(room_name).child(message_id).update({
+        like: no_of_likes
+    });
+}
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,20 +1,20 @@
-//ADD YOUR FIREBASE LINKS HERE
 var firebaseConfig = {
     apiKey: "AIzaSyBEFBRr9LoIKa0n9_WjMZe4BcWc7WbpiJM",
     authDomain: "let-s-chat-web-app-3ee81.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-3ee81-default-rtdb.firebaseio.com",
     projectId: "let-s-chat-web-app-3ee81",
     storageBucket: "let-s-chat-web-app-3ee81.appspot.com",
     messagingSenderId: "211492660817",
     appId: "1:211492660817:web:9029121cc89e7994017a70",
     measurementId: "G-000TDPR43G"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-var user_name = localStorage.getItem("username");
+user_name = localStorage.getItem("username");
 document.getElementById("name_loggedIn").innerHTML = "Welcome " + user_name + "!";
 
-function create_room() {
+function createRoom() {
     room_name = document.getElementById("room_name").value;
     localStorage.setItem("roomname", room_name);
     firebase.database().ref("/").child(room_name).update({
@@ -25,7 +25,7 @@ function create_room() {
 
 function getData() {
     firebase.database().ref("/").on('value', function(snapshot) {
-        document.getElementById("output").innerHTML = "";
+        document.getElementById("other_roomNames").innerHTML = "";
         snapshot.forEach(function(childSnapshot) {
             childKey = childSnapshot.key;
             Room_names = childKey;
@@ -40,4 +40,10 @@ getData();
 function redirectToRoomName() {
     localStorage.setItem("roomname", room_name);
     window.location = "kwitter_page.html";
+}
+
+function logOut() {
+    window.location = "kwitter.html";
+    localStorage.removeItem("username");
+    localStorage.removeItem("roomname");
 }
